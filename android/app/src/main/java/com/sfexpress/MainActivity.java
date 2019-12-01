a252db030ed5f94f8e634f6b027f22eee49ec5c8
zapicojunredexter <zapicojunredexter@yahoo.com>
2019-12-01 16:12:29 +0800

    change build id

--- android/app/src/main/java/com/sfexpress/MainActivity.java
+++ android/app/src/main/java/com/sfexpress/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rnplatform;
+package com.sfexpress;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/sfexpress/MainApplication.java
+++ android/app/src/main/java/com/sfexpress/MainApplication.java
@@ -1,4 +1,4 @@
-package com.rnplatform;
+package com.sfexpress;
 
 import android.app.Application;
 
