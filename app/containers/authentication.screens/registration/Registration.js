c16c3bbed7720cca6eb30bc4e837cc1fcdaa818e
zapicojunredexter <zapicojunredexter@yahoo.com>
2019-12-01 16:50:34 +0800

    added sample service for registration

--- app/containers/authentication.screens/registration/Registration.js
+++ app/containers/authentication.screens/registration/Registration.js
@@ -5,13 +5,40 @@ import {
   View,
   Button,
 } from 'react-native';
+import UserService from '../../../services/user.service';
 
 
 class Registration extends React.Component<> {
+    listener = null;
+    componentDidMount(){
+        console.log('mounted');
+        this.listener = UserService.createListener((result) => {
+            console.log('tua ra man unta', result);
+        });
+    }
+    closeListener = () => {
+        if(this.listener) {
+            console.log('GINACLOSED');
+            this.listener();
+        }
+    };
   render() {
     return (
       <View>
         <Text>You are in REGISTRATION PAGE</Text>
+        <Button
+            title="Create User"
+            onPress={() => {
+                this.props.add({sampleUser:123})
+                    .then(() => alert('DONE'))
+                    .catch(err => alert(err.message))
+            }}
+        />
+        <Button
+            title="CLOSE LISTENER"
+            onPress={this.closeListener}
+        />
+        
       </View>
     );
   }
@@ -19,6 +46,7 @@ class Registration extends React.Component<> {
 const mapStateToProps = store => ({
 });
 const mapDispatchToProps = dispatch => ({
+    add: params => dispatch(UserService.add(params))
 });
 
 export default connect(
---
+++ app/services/user.service.js
@@ -0,0 +1,37 @@
+import firebase from 'react-native-firebase';
+
+export const collection = firebase.firestore().collection('users');
+
+export default class {
+
+    static add = (params) => async (dispatch, getState) => {
+        try {
+            await collection.add({
+                ...params,
+                createdAtMs: firebase.firestore.FieldValue.serverTimestamp(),
+                updatedAtMs: firebase.firestore.FieldValue.serverTimestamp(),
+                deleted: false,
+            });
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    static set = (id, params) => async (dispatch, getState) => {
+        try {
+            await collection.doc(id).set({
+                ...params,
+                updatedAtMs: firebase.firestore.FieldValue.serverTimestamp()
+            });
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    static createListener = (callback = () => {}, query) => {
+        return collection.onSnapshot((result) => {
+            const data = result.docs.map(data => ({id: data.id, ...data.data()}));
+            callback(data);
+        });
+    }
+};
